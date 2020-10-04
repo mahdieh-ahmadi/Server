f7ecfa9945ca6a62201eb515999a77da957a2297
mahdieh-ahmadi <m119.ahmadi@gmail.com>
2020-10-04 21:44:08 +0330

    add /todo and /todo/id

--- app.js
+++ app.js
@@ -4,10 +4,41 @@ const server = express();
 const serverID = process.env.PORT || 2000;
 const middleWare = require('./middleWare')
 
+const todos = [{
+    id : 1,
+    description: 'meet my mom',
+    completed : false
+},{
+    id:2,
+    description : 'go to shop',
+    completed : false
+},{
+    id:3,
+    description : 'do my homework',
+    completed : true
+}]
+
 server.use(middleWare.logger)
 
-server.get('/about' , (req , res) => {
-    res.send('about my team')
+server.get('/todo' , (req , res) => {
+    res.json(todos)
+})
+
+server.get('/todo/:id' , (req , res) => {
+    let idrequest = req.params.id
+    let items = todos.filter((item) => {
+        console.log(item.id == idrequest)
+        if(item.id == idrequest){
+            return item
+        }
+    })
+    
+    if (items.length !==0){
+        res.json(items)
+    }else{
+        res.status(404).send()
+    }
+    
 })
 
 server.use(express.static(__dirname + '/public'))
